1d17c4c04301a5cc7cb1a7459c64e0435af547aa
Flatiron School <andrewsprague@outlook.com>
2018-08-31 15:49:04 -0500

    Done.

---
+++ lib/post.rb
@@ -0,0 +1,23 @@
+class Post
+    
+    attr_reader :title
+    attr_accessor :author
+    
+    @@all = []
+    
+    
+    def initialize (title)
+        @title = title
+
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def author_name
+        self.author.name rescue nil
+    end
+
+end
