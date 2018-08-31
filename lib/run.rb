27bc1005155aadc8194ee111cf1d20fa0608336e
Flatiron School <andrewsprague@outlook.com>
2018-08-31 13:56:58 -0500

    Done.

---
+++ lib/run.rb
@@ -0,0 +1,11 @@
+require_relative './artist'
+require_relative './song'
+require 'pry'
+
+drake = Artist.new('Drake')
+taylor = Artist.new('Taylor Swift')
+
+stay = Song.new('Stay')
+kiki = Song.new('In My Feelings')
+
+binding.pry
